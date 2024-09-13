188196c106207bbea9097c96d6a02d4d9417c616
hacksparkdev <hackspark@outlook.com>
2024-09-13 17:24:34 +0000

    Fixed commit issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 // Initialize Elasticsearch client
-const esClient = new Client({ node: 'http://<elasticsearch-server-ip>:9200' });
+const esClient = new Client({ node: 'http://10.10.20.107:9200' });
 
 // Endpoint to queue a Python module execution
 app.post('/trigger-python-module', async (req, res) => {
