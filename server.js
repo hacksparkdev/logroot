59d5fd01f5b02400de4f3480976a8371672cb7ee
hacksparkdev <hackspark@outlook.com>
2024-09-13 14:02:03 +0000

    Changing project to more module design

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {client} = require {'@elastic/elasticsearch'};
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+//Initialize Elasticsearch client
+const esClient = new Client({node: 'http://10.10.20.107:9200'});
+
+//Endpoint to queue a Python module execution
+app.post('/trigger-python-module', async (req, res) => {
+	const {moduleName} = req.body;
+
+	
+})
