63640ec391258500c54412c7bbf524ee2a1d1a72
hackspark <hackspark@outlook.com>
2024-09-18 12:32:50 +0000

    Added integrity module

--- Python_worker/modules/integrity_monitor.py
+++ Python_worker/modules/integrity_monitor.py
@@ -1,5 +1,5 @@
 import subprocess as sub
 
 def run():
-    result = sub.run(['cmd', '/c', 'dir', stdout=sub.PIPE, text=True])
+    result = sub.run(['cmd', '/c', 'dir'], stdout=sub.PIPE, text=True)
     print(result.stdout)
