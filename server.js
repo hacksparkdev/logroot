125c8cd567b6017a42a452f821ebf8779a0bff68
hackspark <hackspark@outlook.com>
2024-09-18 13:05:24 +0000

    Create visual in nodejs server

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON and form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Add this line
+
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
