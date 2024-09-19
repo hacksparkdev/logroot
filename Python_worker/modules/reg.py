600253b879a2ab3ae0811a9ea7a5c85c8e83a4b0
hacksparkdev <hackspark@outlook.com>
2024-09-19 06:28:39 -0700

    workingon shit

--- Python_worker/modules/reg.py
+++ Python_worker/modules/reg.py
@@ -2,7 +2,7 @@ import re
 import subprocess as sub
 
 
-def get_ip():
+def run():
     cmd = ['ipconfig']
 
     results = sub.run(cmd, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
@@ -10,11 +10,7 @@ def get_ip():
     ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
     ipconfig_result = results.stdout
     matches = re.findall(ip_pattern, ipconfig_result )
-
     for match in matches:
         print(match)
 
-
-def run():
-    return get_ip()
 
