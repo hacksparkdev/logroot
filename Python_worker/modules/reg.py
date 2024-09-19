04a431b7cf79b7d3c25adf9724bdcde117af4a70
hacksparkdev <hackspark@outlook.com>
2024-09-19 06:25:15 -0700

    fixed minor syntax error

--- Python_worker/modules/reg.py
+++ Python_worker/modules/reg.py
@@ -8,7 +8,7 @@ def get_ip():
     results = sub.run(cmd, stdout=sub.PIPE, stderr=sub.PIPE, text=True)
     
     ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
-    ipconfig_result = result.stdout
+    ipconfig_result = results.stdout
     matches = re.findall(ip_pattern, ipconfig_result )
 
     for match in matches:
